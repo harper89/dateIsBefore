672f3971cdce9d64d1a69aebe854dcedd6b529c6
Harper <harperlpeters89@gmail.com>
2025-10-31 09:09:19 -0700

    Added code

---
+++ Main.java
@@ -0,0 +1,32 @@
+public class Main {
+   public static void main(String[] args) {
+   
+   System.out.println(dateIsBefore(2,29,12,11));
+   
+   }
+   public static boolean dateIsBefore(int month1, int day1, int month2, int day2) {
+      if (!(month1>0 && month1<13) || !(month2>0 && month1<13)) {
+         throw new IllegalArgumentException("Bad Month");    
+      }
+      int monthDays[] = {31,29,31,30,31,30,31,31,30,31,30,31};
+      if (!(day1>0 && day1 <= monthDays[month1-1]) || !(day2>0 && day2 <= monthDays[month2-1])) {
+         throw new IllegalArgumentException("Bad Day");
+      }
+      
+      
+      
+      if (month1<month2){
+         return true;
+      } else if (month1>month2) {
+         return false;
+      } else  {
+         if (day1<day2) {
+         return true;
+         } else {
+         return false;
+         }
+      }
+   
+   }
+
+}
